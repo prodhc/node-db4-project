9c7d0c1cf3a8089430b08bca65ea054938ebe472
prodhc <zionywood@gmail.com>
2021-06-10 20:53:53 -0700

    'wip'

--- api/server.js
+++ api/server.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const recipesRouter = require('./recipes/recipes-router')
+const recipesRouter = require('../api/recipes/recipes-router')
 
 const server = express()
 
 server.use(express.json())
 
 
-server.use('api/recipes', recipesRouter)
+server.use('/api/recipes', recipesRouter)
 
 server.use('*', (req, res) => {
     res.json({ api: 'online baby'})
